7a4fb5ec6349c0da3d621e0a64f468213ac5035f
TRENGKR 尹 <taehyun.yoon.k3@mail.toray>
2026-06-25 16:09:39 +0900

    feat: add PWA support

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+})
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig: NextConfig = {}
 
-export default nextConfig;
+module.exports = withPWA(nextConfig)
