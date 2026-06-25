1edaefeaf2cee965045dd0c2b7908e1a25e23156
TRENGKR 尹 <taehyun.yoon.k3@mail.toray>
2026-06-25 17:17:55 +0900

    fix: replace next-pwa with @ducanh2912/next-pwa

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,12 @@
 import type { NextConfig } from 'next'
-const withPWA = require('next-pwa')({
+const withPWA = require('@ducanh2912/next-pwa').default({
   dest: 'public',
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
   disable: process.env.NODE_ENV === 'development',
 })
 
-const nextConfig: NextConfig = {
-  turbopack: {},
-}
+const nextConfig: NextConfig = {}
 
 module.exports = withPWA(nextConfig)
